661ec45a7efd82a8db1ea44cfeda05c24b261f92
Yi Xin <angyixin@gmail.com>
2025-08-17 10:28:31 +0800

    opacity

--- app/polaroid/page.tsx
+++ app/polaroid/page.tsx
@@ -217,7 +217,7 @@ export default function Polaroid() {
                             animationData={instructions[currentInstruction].animation}
                             loop={true}
                             autoplay={true}
-                            style={{ width: '100%', height: '100%' }}
+                            style={{ width: '100%', height: '100%', opacity: 0.8 }}
                         />
                     </div>
                     <div className="text-white font-medium text-base md:text-lg">
